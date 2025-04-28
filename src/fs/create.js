7bf8f06cf305a3f07be49c7d01cf8221b3021ed8
solarv70 <solarv70@gmail.com>
2025-04-28 00:29:13 -0400

    feat: реализовал create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const dirPath = path.join('.', 'files');
+  const filePath = path.join(dirPath, 'fresh.txt');
+
+  try {
+    // check folder 
+    await fs.mkdir(dirPath, { recursive: true });
+
+    // check file
+    try {
+      await fs.access(filePath);
+      throw new Error('FS operation failed'); // error if file allready
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        // Make file
+        await fs.writeFile(filePath, 'I am fresh and young');
+        console.log('Make file');
+      } else {
+        throw err; // error
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 };
 
 await create();
