4418b8e896a943839da6f64afdb154bb43c8f4ec
solarv70 <solarv70@gmail.com>
2025-04-28 00:55:15 -0400

    feat: реализовал rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
-const rename = async () => {
-    // Write your code here 
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const renameFile = async () => {
+  const oldFilePath = path.join('.', 'wrongFilename.txt');
+  const newFilePath = path.join('.', 'properFilename.md');
+
+  try {
+    // check file wrongFilename.txt
+    await fs.access(oldFilePath);
+  } catch {
+    throw new Error('FS operation failed'); // If no file
+  }
+
+  try {
+    // check file properFilename.md
+    await fs.access(newFilePath);
+    throw new Error('FS operation failed'); // File allready
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw new Error('FS operation failed'); // No file
+    }
+  }
+
+  // rename file
+  await fs.rename(oldFilePath, newFilePath);
+  console.log('Файл успешно переименован!');
 };
 
-await rename();
+await renameFile();
