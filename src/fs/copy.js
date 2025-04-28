3c0c41f8217a627d6554e4ffe4f01dab632749fe
solarv70 <solarv70@gmail.com>
2025-04-28 00:47:46 -0400

    feat: реализовал copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,30 @@
-const copy = async () => {
-    // Write your code here 
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const create = async () => {
+  const dirPath = path.join('.', 'files');
+  const filePath = path.join(dirPath, 'fresh.txt');
+
+  try {
+    // check folder 
+    await fs.mkdir(dirPath, { recursive: true });
+
+    // check file
+    try {
+      await fs.access(filePath);
+      throw new Error('FS operation failed'); // error if file allready
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        // Make file
+        await fs.writeFile(filePath, 'I am fresh and young');
+        console.log('Make file');
+      } else {
+        throw err; // error
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 };
 
-await copy();
+await create();
